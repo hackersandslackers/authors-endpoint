283b513ccc0f5544ed506eab23036e355050cb8a
Todd Birchard <toddbirchard@gmail.com>
2019-05-07 05:53:06 -0400

    temporry bug with gcloud

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 ![Python](https://img.shields.io/badge/Python-v3.7.2-blue.svg?logo=python&longCache=true&logoColor=white&colorB=23a8e2&style=flat-square&colorA=36363e)
 ![Flask](https://img.shields.io/badge/Flask-v1.0.2-blue.svg?longCache=true&logo=python&style=flat-square&logoColor=white&colorB=23a8e2&colorA=36363e)
-![BeautifulSoup](https://img.shields.io/badge/beautifulsoup4-v4.6.3-blue.svg?longCache=true&logo=python&longCache=true&style=flat-square&logoColor=white&colorB=23a8e2&colorA=36363e)
 ![Google Cloud Functions](https://img.shields.io/badge/Google--Cloud--Functions-v93-blue.svg?longCache=true&logo=google&longCache=true&style=flat-square&logoColor=white&colorB=23a8e2&colorA=36363e)
 ![GitHub Last Commit](https://img.shields.io/github/last-commit/google/skia.svg?style=flat-square&colorA=36363e)
 [![GitHub Issues](https://img.shields.io/github/issues/hackersandslackers/authors-endpoint.svg?style=flat-square&colorA=36363e&logo=Github)](https://github.com/hackersandslackers/authors-endpoint/issues)
--- main.py
+++ main.py
@@ -14,16 +14,26 @@ def main(request):
     3. Convert metadata to JSON object.
     4. Return response.
     """
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Max-Age': '3600',
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    }
-    author_slug = request.args['author']
-    author = get_authors_from_database(author_slug)
-    return make_response(str(author), 200, headers)
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+    if request.method == 'GET':
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+        }
+        author_slug = request.args['author']
+        author = get_authors_from_database(author_slug)
+        return make_response(str(author), 200, headers)
 
 
 if __name__ == '__main__':
